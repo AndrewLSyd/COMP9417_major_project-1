2d6af5f02ad48211312b22e9c2d15c5b876b264f
Andrew Lau <andrew.ondu.lau@gmail.com>
2019-11-12 23:06:04 +1100

    feature engineering gps wifi - updated code, uploaded CSV
    * uploaded CSV
    * added bearing feature
    * capped time_elapsed
    * capped dist_moved
    * added time spent in top locations

--- feature_engineering_gps_wifi.R
+++ feature_engineering_gps_wifi.R
@@ -42,11 +42,19 @@
 # bearing - grouped into N E S W for outdoor data only
 
 # CONSTANTS --------------------------------------------------------------------
+FPATH_OUT <- "preprocessed_data/features_gps_wifi.csv"
 DATE_TERM_START <- strptime("18.03.2013", format = "%d.%m.%Y")
+# value used when time_elapsed cannot be calculated. 20m represents the median
+# value of time elapsed
+TIME_ELAPSED_IMPUTE <- 1200
+TIME_ELAPSED_CAP <- 1800
+DIST_MOVED_CAP <- 1.045034e-02
 
-# options(dplyr.width = Inf)
+options(dplyr.width = Inf)
 # head(input_data$gps)
 # view(input_data$gps)
+# input_data_backup = input_data
+
 
 # HELPER FUNCTIONS -------------------------------------------------------------
 spread_join <- function(df, var, var_str, first=FALSE){
@@ -99,26 +107,31 @@ create_time_elapsed <- function(df){
   df <- df %>%
     arrange(uid, time) %>%  # sort by uid then time
     mutate(time_elapsed = time - lag(time)) %>%
-    replace_na(list(time_elapsed = 1200))
+    replace_na(list(time_elapsed = TIME_ELAPSED_IMPUTE))
 
   # cleanup
   # the lagged time value for the first entry for each uid will lead to
-  # non-sensical values. assume the time elapsed for the first and last entries is
-  # 20m
+  # non-sensical values. assume the time elapsed for the first and last entries
+  # is 20m
   # replacing first obs/last obs (for each uid), NANs and negative with 1200s
   df <- create_first_last_obs(df)
-  df <- df %>% replace_na(list(time_elapsed = 1200))
-  df[df$time_elapsed < 0, ]$time_elapsed = 1200
-  df[df$first_obs, ]$time_elapsed = 1200
+  df <- df %>% replace_na(list(time_elapsed = TIME_ELAPSED_IMPUTE))
+  df[df$time_elapsed < 0, ]$time_elapsed = TIME_ELAPSED_IMPUTE
+  df[df$first_obs, ]$time_elapsed = TIME_ELAPSED_IMPUTE
 
+  # there are non-sensically high values for time elapsed, finding something
+  # reasonable to cap it at. Have gone with 30m which is ~98th percentile
+  df[df$time_elapsed > TIME_ELAPSED_CAP, ]$time_elapsed = TIME_ELAPSED_CAP
   df <- select(df, -one_of(c("first_obs", "last_obs")))
   return(df)
 }
 
+
 input_data$gps <- create_time_elapsed(input_data$gps)
 input_data$wifi_location <- create_time_elapsed(input_data$wifi_location)
 
-head(input_data$wifi_location)
+# quantile(input_data$gps$time_elapsed, c(.50, .75, .9, 0.95, 0.975, .98, .99,
+# .995, 1))
 
 # PREPARING DATA: distance moved -----------------------------------------------
 input_data$gps <- create_first_last_obs(input_data$gps)
@@ -132,6 +145,11 @@ input_data$gps <- input_data$gps %>%
   replace_na(list(dist_moved = 0)) %>%
   select(-one_of(c("first_obs", "last_obs", "dist_moved_lat",
                    "dist_moved_long")))
+  # capping dist_moved at the value of the 98th percentile to remove
+  # non-sensical values
+  # quantile(input_data$gps$dist_moved, c(.50, .75, .9, 0.95, 0.975, .98, .99, .995, 1))
+
+input_data$gps[input_data$gps$dist_moved > DIST_MOVED_CAP, ]$dist_moved = DIST_MOVED_CAP
 
 # FEATURES: speed -----------------------------------------------------------
 # average speed in each week
@@ -139,6 +157,7 @@ speed_mean <- input_data$gps %>%
     group_by(uid, week) %>%
     summarise(speed_mean = mean(speed, na.rm = TRUE))
 features_wifi_gps <- spread_join(tibble(), speed_mean, "speed_mean",  TRUE)
+# median speed in each week is of no value as the median value is 0 for most uid
 
 # max speed in each week
 speed_max <- input_data$gps %>%
@@ -191,7 +210,7 @@ indoor_dist <- input_data$gps %>%
   summarise(indoor_dist = sum(dist_moved, na.rm = TRUE))
 features_wifi_gps <- spread_join(features_wifi_gps, indoor_dist,
                                  "indoor_dist")
-# distance moved outdoorss using provider == "gps" as a proxy
+# distance moved outdoors using provider == "gps" as a proxy
 outdoors_dist <- input_data$gps %>%
   group_by(uid, week) %>%
   filter(provider != "gps") %>%
@@ -226,6 +245,7 @@ altitude_min <- input_data$gps %>%
   summarise(altitude_min = min(altitude, na.rm = TRUE))
 features_wifi_gps <- spread_join(features_wifi_gps, altitude_min,
                                  "altitude_min")
+
 # FEATURES : locations ---------------------------------------------------------
 # number of different locations visited each week
 location_count <- input_data$wifi_location %>%
@@ -236,8 +256,88 @@ location_count <- input_data$wifi_location %>%
 features_wifi_gps <- spread_join(features_wifi_gps, location_count,
                                  "location_count")
 
-# FEATURES : bearing ---------------------------------------------------------
-# bearing_compass <- input_data$gps %>%
-#   transmute(bearing_compass = round(bearing / 90)
-#
-# glimpse(features_wifi_gps)
+# amount of time spent in the 5 most common locations
+# top 10 most common locations are:
+# input_data$wifi_location %>%
+#   group_by(location) %>%
+#   tally() %>%
+#   arrange(desc(n))
+
+# in[north-main]	284288
+# in[sudikoff]	267098
+# in[mclaughlin]	143585
+# in[north-park]	118475
+# in[massrow]	106341
+
+location_1_time <- input_data$wifi_location %>%
+  filter(location == "in[north-main]") %>%
+  group_by(uid, week) %>%
+  summarise(location_1_time = sum(time_elapsed, na.rm = TRUE))
+features_wifi_gps <- spread_join(features_wifi_gps, location_1_time,
+                                 "location_1_time")
+
+location_2_time <- input_data$wifi_location %>%
+  filter(location == "in[sudikoff]") %>%
+  group_by(uid, week) %>%
+  summarise(location_2_time = sum(time_elapsed, na.rm = TRUE))
+features_wifi_gps <- spread_join(features_wifi_gps, location_2_time,
+                                 "location_2_time")
+
+location_3_time <- input_data$wifi_location %>%
+  filter(location == "in[mclaughlin]") %>%
+  group_by(uid, week) %>%
+  summarise(location_3_time = sum(time_elapsed, na.rm = TRUE))
+features_wifi_gps <- spread_join(features_wifi_gps, location_3_time,
+                                 "location_3_time")
+
+location_4_time <- input_data$wifi_location %>%
+  filter(location == "in[north-park]") %>%
+  group_by(uid, week) %>%
+  summarise(location_4_time = sum(time_elapsed, na.rm = TRUE))
+features_wifi_gps <- spread_join(features_wifi_gps, location_4_time,
+                                 "location_4_time")
+
+location_5_time <- input_data$wifi_location %>%
+  filter(location == "in[massrow]") %>%
+  group_by(uid, week) %>%
+  summarise(location_5_time = sum(time_elapsed, na.rm = TRUE))
+features_wifi_gps <- spread_join(features_wifi_gps, location_5_time,
+                                 "location_5_time")
+
+# FEATURES : bearing -----------------------------------------------------------
+# only valid if network is GPS
+# group into N E S W
+bearing <- input_data$gps %>%
+  mutate(bearing = round(bearing / 90) %% 4) %>%
+  filter(provider == "gps")
+
+bearing_north_time <- bearing %>%
+  filter(bearing == 0) %>%
+  group_by(uid, week) %>%
+  summarise(bearing_north_time = sum(time_elapsed, na.rm = TRUE))
+features_wifi_gps <- spread_join(features_wifi_gps, bearing_north_time,
+                                 "bearing_north_time")
+
+bearing_east_time <- bearing %>%
+  filter(bearing == 1) %>%
+  group_by(uid, week) %>%
+  summarise(bearing_east_time = sum(time_elapsed, na.rm = TRUE))
+features_wifi_gps <- spread_join(features_wifi_gps, bearing_east_time,
+                                 "bearing_east_time")
+
+bearing_south_time <- bearing %>%
+  filter(bearing == 2) %>%
+  group_by(uid, week) %>%
+  summarise(bearing_south_time = sum(time_elapsed, na.rm = TRUE))
+features_wifi_gps <- spread_join(features_wifi_gps, bearing_south_time,
+                                 "bearing_south_time")
+
+bearing_west_time <- bearing %>%
+  filter(bearing == 3) %>%
+  group_by(uid, week) %>%
+  summarise(bearing_west_time = sum(time_elapsed, na.rm = TRUE))
+features_wifi_gps <- spread_join(features_wifi_gps, bearing_west_time,
+                                 "bearing_west_time")
+
+# FEATURES : export ------------------------------------------------------------
+write_csv(features_wifi_gps, FPATH_OUT)
