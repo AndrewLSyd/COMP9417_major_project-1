94ccd2d8774c0a8bb6e33fbc15546dc839c75e09
Andrew Lau <andrew.ondu.lau@gmail.com>
2019-11-24 00:21:43 +1100

    helper_functions,py - updated documentation

--- helper_functions.py
+++ helper_functions.py
@@ -12,22 +12,26 @@ from matplotlib.ticker import FormatStrFormatter
 
 from matplotlib.ticker import FormatStrFormatter
 
-def quantile_plot(y_actual, y_pred, quantiles=10, scale=1.0, title=None):
+def quantile_plot(y_true, y_pred, quantiles=10, scale=1.0, title=None):
     """ 
-    description: plots a decile plot based on deciling by y_pred. returns the df with
-    the deciles.
+    description: plots a quantile plot based on sorting observations by y_pred
+        and grouping them into quantiles.
     inputs:
-        y_actual - array like object of actuals
+        y_true - array like object of actuals
         y_pred - array like object of predicted values
+        quantiles - number of quantiles/bins to separate the data
+        scale - the predicted values can be scaled to focus on the ability of
+            the model to rank the observations
+        title - plot title
     returns:
-        DataFrame with quantiles
+        DataFrame with y_true and y_pred grouped into quantiles sorted by y_pred
     """
     if scale == "auto":
-        scale_used = sum(y_actual) / sum(y_pred)
+        scale_used = sum(y_true) / sum(y_pred)
     else:
         scale_used = scale
 
-    df = pd.DataFrame({'Actual':np.array(y_actual).reshape(-1),
+    df = pd.DataFrame({'Actual':np.array(y_true).reshape(-1),
                                   'Predicted':np.array(y_pred).reshape(-1) * scale_used})
     df = df.groupby(pd.qcut(df.rank(method="first").loc[:, "Predicted"], quantiles)).mean()
     df.loc[:, "Quantile"] = (list(range(1, quantiles + 1)))
@@ -35,8 +39,7 @@ def quantile_plot(y_actual, y_pred, quantiles=10, scale=1.0, title=None):
 
     df.plot()
     plt.gca().xaxis.set_major_formatter(FormatStrFormatter('%d'))
-    plt.gca().set_ylabel("Target")
-    # plt.xticks(rotation=90)
+    plt.gca().set_ylabel("Target")    
     plt.gca().xaxis.set_major_locator(plt.MaxNLocator(quantiles))
     
     if title:
@@ -101,46 +104,3 @@ def get_best_features_tree(tree_model, column_names):
     tree_model_feature_imp = pd.DataFrame({'feature':column_names, 'importance':tree_model.feature_importances_})
     tree_model_feature_imp = tree_model_feature_imp.sort_values('importance', ascending=False)
     return tree_model_feature_imp
-
-# WORK IN PROGRESS - DONT USE
-# def shap_feature_imp(df_shap,df, num_feat=20):
-#     """
-#     creates a shap plot of feature importance with bars coloured indicated
-#     direction of effect. Red = positively correlated with target, Blue = negatively
-#     correlated with target
-#     inputs:
-#         df_shap - shap object
-#         df - dataframe with features
-#         corr_thresh - the threshold at which we include the feature
-#     adapted from
-#     https://towardsdatascience.com/explain-your-model-with-the-shap-values-bc36aac4de3d
-#     """
-#     shap_v = pd.DataFrame(df_shap)
-#     feature_list = df.columns
-#     shap_v.columns = feature_list
-#     #    df_v = df.copy().reset_index().drop('index',axis=1)
-#     df_v = df.copy().reset_index()
-    
-#     # Determine the correlation in order to plot with different colors
-#     corr_list = []
-#     feature_list_selected = []
-#     for i in feature_list:
-#         b = np.corrcoef(shap_v[i],df_v[i])[1][0]
-#         corr_list.append(b)
-#         feature_list_selected.append(i)
-    
-#     corr_df = pd.concat([pd.Series(feature_list_selected),pd.Series(corr_list)],axis=1).fillna(0)
-#     # Make a data frame. Column 1 is the feature, and Column 2 is the correlation coefficient
-#     corr_df.columns  = ['Variable','Corr']
-#     corr_df['Sign'] = np.where(corr_df['Corr']>0,'red','blue')
-    
-#     # Plot it
-#     shap_abs = np.abs(shap_v)
-#     k=pd.DataFrame(shap_abs.mean()).reset_index()
-#     k.columns = ['Variable','SHAP_abs']
-#     k2 = k.merge(corr_df,left_on = 'Variable',right_on='Variable',how='inner')
-#     k2 = k2.sort_values(by='SHAP_abs',ascending = True)
-#     k2 = k2.loc[num_feat, :]
-#     colorlist = k2['Sign']
-#     ax = k2.plot.barh(x='Variable',y='SHAP_abs',color = colorlist, figsize=(5,6),legend=False)
-#     ax.set_xlabel("SHAP Value (Red = Positive Impact)")
